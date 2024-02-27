ff92cda62d150bb3ff8afafe88fd431ad403b076
gerej <gergely.toth3@icellmobilsoft.hu>
2024-02-27 10:04:58 +0100

    Simon game created

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,3 +1,8 @@
-$("h1").css("color", "red");
+$(document).on("click", function(){
+    $("h1").css("color", "purple");
+})
 
+$(document).keydown(function(event){
+    $("h1").text(event.key);
+})
 
