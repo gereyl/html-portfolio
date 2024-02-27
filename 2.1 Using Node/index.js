ffc509b18a4848796b2d5048b99bab991fc2e4ea
gerej <gergely.toth3@icellmobilsoft.hu>
2024-02-27 15:56:11 +0100

    qr generator added

---
+++ 2.1 Using Node/index.js
@@ -0,0 +1,63 @@
+console.log("Hello from node!");
+
+/*
+Tóth Gergely3@tothgergely3 MINGW64 ~/Desktop/proj/uploaded/html-portfolio-1 (main)
+$ node -v
+v20.11.1
+
+Tóth Gergely3@tothgergely3 MINGW64 ~/Desktop/proj/uploaded/html-portfolio-1 (main)
+$ %node -v
+bash: fg: %node: no such job
+
+Tóth Gergely3@tothgergely3 MINGW64 ~/Desktop/proj/uploaded/html-portfolio-1 (main)
+$ node
+Welcome to Node.js v20.11.1.
+Type ".help" for more information.
+> .help
+.break    Sometimes you get stuck, this gets you out
+.clear    Alias for .break
+.editor   Enter editor mode
+.exit     Exit the REPL
+.help     Print this help message
+.load     Load JS from a file into the REPL session
+.save     Save all evaluated commands in this REPL session to a file
+
+Press Ctrl+C to abort current expression, Ctrl+D to exit the REPL
+> .exit
+
+Tóth Gergely3@tothgergely3 MINGW64 ~/Desktop/proj/uploaded/html-portfolio-1 (main)
+$ node
+Welcome to Node.js v20.11.1.
+Type ".help" for more information.
+>
+(To exit, press Ctrl+C again or Ctrl+D or type .exit)
+> 5+8
+13
+> let a = 3
+undefined
+> a+12
+15
+>
+(To exit, press Ctrl+C again or Ctrl+D or type .exit)
+>
+
+Tóth Gergely3@tothgergely3 MINGW64 ~/Desktop/proj/uploaded/html-portfolio-1 (main)
+$ cd 'c:/Users/Tóth Gergely3/Desktop/proj/uploaded/html-portfolio-1/2.1 Using Node'
+
+Tóth Gergely3@tothgergely3 MINGW64 ~/Desktop/proj/uploaded/html-portfolio-1/2.1 Using Node (main)
+$ node index.js
+
+Tóth Gergely3@tothgergely3 MINGW64 ~/Desktop/proj/uploaded/html-portfolio-1/2.1 Using Node (main)
+$ node index.js
+
+Tóth Gergely3@tothgergely3 MINGW64 ~/Desktop/proj/uploaded/html-portfolio-1/2.1 Using Node (main)
+$ node index.js
+Hello from node!
+
+Tóth Gergely3@tothgergely3 MINGW64 ~/Desktop/proj/uploaded/html-portfolio-1/2.1 Using Node (main)
+$
+
+touch text.txt
+
+
+*/
